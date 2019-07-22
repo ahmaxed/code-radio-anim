88cce934a8aa45d9cf16df000a3341a8b0e1604d
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2019-07-22 10:40:47 +0300

    feat: make play and volume fully functional

--- player.js
+++ player.js
@@ -142,7 +142,7 @@ class CodeRadio {
           this.currentSong = np.now_playing.song;
           this.played_at = np.now_playing.played_at;
           this.duration = np.now_playing.duration;
-          this.meta.listeners.textContent = `Listeners: ${
+          this.meta.listeners.textContent = `coders listening right now: ${
             np.listeners.current
           }`;
           if (!this._progressInterval) {
@@ -166,8 +166,8 @@ class CodeRadio {
    * Yay, let's get some keyboard shortcuts in this tool
    */
   setupEventListeners() {
-    document.addEventListener("keydown", evt => this.keyboardControl(evt));
-    document.getElementById("playToggle").onclick = this.handleToggleButton;
+    // commented for now to find a good way to communicate with slier on index.html
+    //document.addEventListener("keydown", evt => this.keyboardControl(evt));
 
     // In order to get around some mobile browser limitations, we can only generate a lot
     // of the audio context stuff AFTER the audio has been triggered. We can't see it until
